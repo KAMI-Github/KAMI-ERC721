229e33e61fceafa3819a51164358ac54a7fe0ee2
Paul Stinchcombe <paul.stinchcombe@gmail.com>
2024-12-06 11:01:47 +0800

    Converted to use ERC20 token for payments and added documentation

--- contracts/ERC721-C.sol
+++ contracts/ERC721-C.sol
@@ -1,28 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+// Importing necessary OpenZeppelin contracts for upgradeable ERC721 token
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+// ERC721CUpgradeable contract definition
 contract ERC721CUpgradeable is ERC721Upgradeable, ERC721URIStorageUpgradeable, ERC721BurnableUpgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+    // Define roles for access control
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
+    // State variables for royalty management
     address[] private _royaltyReceivers;
     uint256[] private _royaltyShares;
     uint256 private _secondaryRoyaltyPercentage;
 
-    
+    // Mappings for token data
     mapping(uint256 => string) private _tokenURIs;
     mapping(uint256 => address) private _tokenAuthors;
     mapping(uint256 => uint256) private _tokenPrices;
     mapping(uint256 => address) private _tokenOwners;
 
-    function initialize(string memory name, string memory symbol, address[] memory royaltyReceivers, uint256[] memory royaltyShares, uint256 secondaryRoyaltyPercentage) public initializer {
+    // ERC20 token used for payments
+    IERC20 private _paymentToken;
+
+    // Initialize the contract with necessary parameters
+    /**
+     * @dev Initializes the contract with the given parameters.
+     * @param name The name of the token.
+     * @param symbol The symbol of the token.
+     * @param royaltyReceivers The addresses that will receive royalties.
+     * @param royaltyShares The shares of royalties for each receiver.
+     * @param secondaryRoyaltyPercentage The percentage of royalties for secondary sales.
+     * @param paymentTokenAddress The address of the ERC20 token used for payments.
+     */
+    function initialize(
+        string memory name,
+        string memory symbol,
+        address[] memory royaltyReceivers,
+        uint256[] memory royaltyShares,
+        uint256 secondaryRoyaltyPercentage,
+        address paymentTokenAddress
+    ) public initializer {
+        // Initialize inherited contracts
         __ERC721_init(name, symbol);
         __ERC721URIStorage_init();
         __ERC721Burnable_init();
@@ -30,6 +56,7 @@ contract ERC721CUpgradeable is ERC721Upgradeable, ERC721URIStorageUpgradeable, E
         __AccessControl_init();
         __UUPSUpgradeable_init();
 
+        // Validate royalty shares
         require(royaltyReceivers.length == royaltyShares.length, "Receivers and shares length mismatch");
         uint256 totalShares = 0;
         for (uint256 i = 0; i < royaltyShares.length; i++) {
@@ -37,28 +64,55 @@ contract ERC721CUpgradeable is ERC721Upgradeable, ERC721URIStorageUpgradeable, E
         }
         require(totalShares == 10000, "Total shares must be 10000");
 
+        // Set royalty data
         _royaltyReceivers = royaltyReceivers;
         _royaltyShares = royaltyShares;
         _secondaryRoyaltyPercentage = secondaryRoyaltyPercentage;
 
+        // Set payment token
+        _paymentToken = IERC20(paymentTokenAddress);
+
+        // Setup roles for access control
         setupRoles();
     }
 
+    // Set the secondary royalty percentage
+    /**
+     * @dev Sets the secondary royalty percentage.
+     * @param share The new secondary royalty percentage (max 1000).
+     */
     function setSecondaryRoyaltyPercentage(uint256 share) public onlyRole(MINTER_ROLE) {
         require(share <= 1000, "Percentage must be less than 1000");
         _secondaryRoyaltyPercentage = share;
     }
 
+    // Get the secondary royalty percentage
+    /**
+     * @dev Returns the secondary royalty percentage.
+     * @return The current secondary royalty percentage.
+     */
     function getSecondaryRoyaltyPercentage() public view returns (uint256) {
         return _secondaryRoyaltyPercentage;
     }
 
+    // Setup roles for access control
+    /**
+     * @dev Internal function to setup roles for access control.
+     */
     function setupRoles() internal {
         AccessControlUpgradeable._grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         AccessControlUpgradeable._grantRole(MINTER_ROLE, msg.sender);
         AccessControlUpgradeable._grantRole(UPGRADER_ROLE, msg.sender);
     }
 
+    // Mint a new token
+    /**
+     * @dev Mints a new token with the given parameters.
+     * @param to The address to mint the token to.
+     * @param tokenId The ID of the token to mint.
+     * @param uri The URI of the token.
+     * @param price The price of the token.
+     */
     function mint(address to, uint256 tokenId, string memory uri, uint256 price) public onlyRole(MINTER_ROLE) {
         _mint(to, tokenId);
         _tokenURIs[tokenId] = uri;
@@ -67,41 +121,89 @@ contract ERC721CUpgradeable is ERC721Upgradeable, ERC721URIStorageUpgradeable, E
         _tokenOwners[tokenId] = msg.sender;
     }
 
+    // Set the price of a token
+    /**
+     * @dev Sets the price of a token.
+     * @param tokenId The ID of the token.
+     * @param price The new price of the token.
+     */
     function setPrice(uint256 tokenId, uint256 price) public onlyRole(MINTER_ROLE) {
         _tokenPrices[tokenId] = price;
     }
 
+    // Get the price of a token
+    /**
+     * @dev Returns the price of a token.
+     * @param tokenId The ID of the token.
+     * @return The current price of the token.
+     */
     function getPrice(uint256 tokenId) public view returns (uint256) {
         return _tokenPrices[tokenId];
     }
 
-    function buy(uint256 tokenId) public payable {
+    // Buy a token
+    /**
+     * @dev Allows a user to buy a token.
+     * @param tokenId The ID of the token to buy.
+     */
+    function buy(uint256 tokenId) public {
         require(_tokenPrices[tokenId] > 0, "Token not for sale");
-        require(msg.value >= _tokenPrices[tokenId], "Insufficient funds sent");
-        uint256 royalties = distributeRoyalties(msg.value, tokenId);
-        if(msg.value > royalties) {
-            payable(_tokenOwners[tokenId]).transfer(msg.value - royalties);
+        uint256 price = _tokenPrices[tokenId];
+        require(_paymentToken.transferFrom(msg.sender, address(this), price), "Token transfer failed");
+
+        uint256 royalties = distributeRoyalties(price, tokenId);
+        if(price > royalties) {
+            require(_paymentToken.transfer(_tokenOwners[tokenId], price - royalties), "Token transfer to owner failed");
         }
         _transfer(_tokenOwners[tokenId], msg.sender, tokenId);
         _tokenOwners[tokenId] = msg.sender;
     }
 
+    // Authorize contract upgrade
+    /**
+     * @dev Authorizes an upgrade to a new implementation.
+     * @param newImplementation The address of the new implementation.
+     */
     function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADER_ROLE) {}
 
-
+    // Override tokenURI function to use ERC721URIStorage
+    /**
+     * @dev Returns the URI of a token.
+     * @param tokenId The ID of the token.
+     * @return The URI of the token.
+     */
     function tokenURI(uint256 tokenId) public view override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
+    // Set the URI of a token
+    /**
+     * @dev Sets the URI of a token.
+     * @param tokenId The ID of the token.
+     * @param uri The new URI of the token.
+     */
     function setTokenUri(uint256 tokenId, string memory uri) public onlyRole(MINTER_ROLE) {
         _setTokenURI(tokenId, uri);
     }
 
+    // Override supportsInterface to include all inherited interfaces
+    /**
+     * @dev Checks if the contract supports a given interface.
+     * @param interfaceId The ID of the interface.
+     * @return True if the interface is supported, false otherwise.
+     */
     function supportsInterface(bytes4 interfaceId) public view override(ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, AccessControlUpgradeable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    function distributeRoyalties(uint256 totalReceived, uint256 tokenId) public payable returns (uint256) {
+    // Distribute royalties to receivers
+    /**
+     * @dev Distributes royalties to the royalty receivers.
+     * @param totalReceived The total amount received from the sale.
+     * @param tokenId The ID of the token sold.
+     * @return The total amount distributed as royalties.
+     */
+    function distributeRoyalties(uint256 totalReceived, uint256 tokenId) public returns (uint256) {
         require(totalReceived > 0, "No funds to distribute");
         
         uint256 sp = 10000;
@@ -113,13 +215,21 @@ contract ERC721CUpgradeable is ERC721Upgradeable, ERC721URIStorageUpgradeable, E
         for (uint256 i = 0; i < _royaltyReceivers.length; i++) {
             uint256 share = (_royaltyShares[i] * sp) / 10000;
             uint256 payment = (totalReceived * share) / 10000;
-            payable(_royaltyReceivers[i]).transfer(payment);
+            require(_paymentToken.transfer(_royaltyReceivers[i], payment), "Token transfer to royalty receiver failed");
             totalDistributed += payment;
         }
 
         return totalDistributed;
     }
 
+    // Override _update function for ERC721Enumerable
+    /**
+     * @dev Updates the token ownership and balance.
+     * @param to The address to transfer the token to.
+     * @param tokenId The ID of the token.
+     * @param auth The address of the author.
+     * @return The address of the previous owner.
+     */
     function _update(
         address to,
         uint256 tokenId,
@@ -128,6 +238,12 @@ contract ERC721CUpgradeable is ERC721Upgradeable, ERC721URIStorageUpgradeable, E
         return super._update(to, tokenId, auth);
     }
 
+    // Override _increaseBalance function for ERC721Enumerable
+    /**
+     * @dev Increases the balance of an account.
+     * @param account The address of the account.
+     * @param value The amount to increase the balance by.
+     */
     function _increaseBalance(
         address account,
         uint128 value
--- test/ERC721-C.ts
+++ test/ERC721-C.ts
@@ -1,12 +1,20 @@
 import { expect } from 'chai';
 import { ethers, upgrades } from 'hardhat';
-import { ethers as ethersLib } from 'ethers';
-import { Contract, Signer } from 'ethers';
+import { ethers as ethersLib, BigNumberish, Contract } from 'ethers';
+import { Signer } from 'ethers';
 
 type ERC721C = Contract & {
-	mint: (to: string, tokenId: number, uri: string) => Promise<any>;
+	mint: (to: string, tokenId: number, uri: string, price: BigNumberish) => Promise<any>;
 	distributeRoyalties: (options: { value: any }) => Promise<any>;
 	address: string;
+	getPrice: (tokenId: number) => Promise<ethersLib.BigNumberish>;
+	setPrice: (tokenId: number, price: BigNumberish) => Promise<any>;
+	getSecondaryRoyaltyPercentage: () => Promise<number>;
+	setSecondaryRoyaltyPercentage: (percentage: number) => Promise<any>;
+};
+
+type KamiUSD = Contract & {
+	approve: (spender: string, amount: BigNumberish) => Promise<any>;
 };
 
 describe('ERC721CUpgradeable', function () {
@@ -15,22 +23,39 @@ describe('ERC721CUpgradeable', function () {
 	let addr2: Signer;
 	let addr3: Signer;
 	let erc721c: ERC721C;
+	let kamiUSD: KamiUSD;
 
 	beforeEach(async function () {
 		[owner, addr1, addr2, addr3] = await ethers.getSigners();
 
+		console.log('Owner Address:', await owner.getAddress());
+
+		// Deploy KamiUSD token
+		const KamiUSDFactory = await ethers.getContractFactory('KamiUSD');
+		kamiUSD = (await KamiUSDFactory.deploy()) as unknown as KamiUSD;
+		await kamiUSD.waitForDeployment();
+
 		const royaltyReceivers = [await addr1.getAddress(), await addr2.getAddress()];
 		const royaltyShares = [5000, 5000];
+		const secondaryRoyaltyPercentage = 500;
 
 		const ERC721CFactory = await ethers.getContractFactory('ERC721CUpgradeable');
-		erc721c = (await upgrades.deployProxy(ERC721CFactory, ['TestToken', 'TTK', royaltyReceivers, royaltyShares], {
-			initializer: 'initialize',
-		})) as typeof erc721c;
-		// await erc721c.deployed();
+		erc721c = (await upgrades.deployProxy(
+			ERC721CFactory,
+			['TestToken', 'TTK', royaltyReceivers, royaltyShares, secondaryRoyaltyPercentage, kamiUSD.address],
+			{
+				initializer: 'initialize',
+			}
+		)) as typeof erc721c;
 		await erc721c.waitForDeployment();
 
 		await erc721c.grantRole(await erc721c.MINTER_ROLE(), await owner.getAddress());
 		await erc721c.grantRole(await erc721c.UPGRADER_ROLE(), await owner.getAddress());
+
+		// Distribute some KamiUSD tokens to addr1 and addr2 for testing
+		await kamiUSD.transfer(await addr1.getAddress(), ethersLib.parseUnits('1000', 18));
+		await kamiUSD.transfer( await addr2.getAddress(), ethersLib.parseUnits( '1000', 18 ) );
+		console.log(await kamiUSD.connect(addr1).)
 	});
 
 	it('should assign roles correctly', async function () {
@@ -40,8 +65,9 @@ describe('ERC721CUpgradeable', function () {
 	});
 
 	it('should allow minting by minter', async function () {
-		await erc721c.mint(await addr1.getAddress(), 1, 'https://token-uri.com/1');
+		await erc721c.mint(await addr1.getAddress(), 1, 'https://token-uri.com/1', ethersLib.parseUnits('1.0', 18));
 		expect(await erc721c.ownerOf(1)).to.equal(await addr1.getAddress());
+		expect(await erc721c.getPrice(1)).to.equal(ethersLib.parseUnits('1.0', 18));
 	});
 
 	it('set token uri correctly', async function () {
@@ -50,25 +76,44 @@ describe('ERC721CUpgradeable', function () {
 	});
 
 	it('should not allow minting by non-minter', async function () {
-		expect(await erc721c.mint(await addr1.getAddress(), 2, 'https://token-uri.com/2')).to.be.revertedWith('AccessControl: account');
+		expect(
+			await erc721c.mint(await addr1.getAddress(), 2, 'https://token-uri.com/2', ethersLib.parseUnits('1.0', 18))
+		).to.be.revertedWith('AccessControl: account');
 	});
 
 	it('should distribute royalties correctly', async function () {
 		const initialBalance1 = await ethers.provider.getBalance(await addr1.getAddress());
 		const initialBalance2 = await ethers.provider.getBalance(await addr2.getAddress());
 
-		await (erc721c.connect(owner) as ERC721C).distributeRoyalties({ value: ethersLib.parseEther('1.0') });
+		await (erc721c.connect(owner) as ERC721C).distributeRoyalties({ value: ethersLib.parseUnits('1.0', 18) });
 
 		const finalBalance1 = await ethers.provider.getBalance(await addr1.getAddress());
 		const finalBalance2 = await ethers.provider.getBalance(await addr2.getAddress());
 
-		expect(finalBalance1 - initialBalance1).to.equal(ethersLib.parseEther('0.5'));
-		expect(finalBalance2 - initialBalance2).to.equal(ethersLib.parseEther('0.5'));
+		expect(finalBalance1 - initialBalance1).to.equal(ethersLib.parseUnits('0.5', 18));
+		expect(finalBalance2 - initialBalance2).to.equal(ethersLib.parseUnits('0.5', 18));
 	});
 
 	it('should allow upgrades by upgrader', async function () {
 		const ERC721CUpgradeableV2 = await ethers.getContractFactory('ERC721CUpgradeable');
 		const erc721c_v2 = await upgrades.upgradeProxy(await erc721c.getAddress(), ERC721CUpgradeableV2);
 		expect(erc721c_v2.address).to.equal(await erc721c.address);
+	});
+
+	it('should allow setting and getting secondary royalty percentage', async function () {
+		await erc721c.setSecondaryRoyaltyPercentage(300);
+		expect(await erc721c.getSecondaryRoyaltyPercentage()).to.equal(300);
+	});
+
+	it('should handle token purchase with KamiUSD', async function () {
+		await erc721c.mint(await addr1.getAddress(), 2, 'https://token-uri.com/2', ethersLib.parseUnits('1.0', 18));
+		await (erc721c.connect(addr1) as ERC721C).setPrice(2, ethersLib.parseUnits('1.0', 18));
+
+		// Approve ERC721 contract to spend KamiUSD on behalf of addr2
+		await (kamiUSD.connect(addr2) as KamiUSD).approve(erc721c.address, ethersLib.parseUnits('1.0', 18));
+
+		// Simulate token purchase
+		await (erc721c.connect(addr2) as ERC721C).buy(2);
+		expect(await erc721c.ownerOf(2)).to.equal(await addr2.getAddress());
 	});
 });
